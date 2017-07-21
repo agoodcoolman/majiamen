fbbdb61f51276c1938b822518412cee0755807db
jin <agoodcoolman@163.com>
2017-07-21 21:03:42 +0800

    1.修改弹窗的问题

--- www/js/login/login.js
+++ www/js/login/login.js
@@ -2,9 +2,52 @@
  * Created by jin on 2017/7/21.
  */
 angular.module('mjm.login', [])
-  .controller('loginCtrl', function ($scope) {
+  .controller('loginCtrl', function ($scope, $ionicPopover) {
     $scope.login = function () {
-
+        // 登陆
     };
+
+    $scope.showTheChoice = $ionicPopover.fromTemplateUrl("js/login/SecurityQuestionPopover.html", {
+        scope:$scope
+    }).then(function (popover) {
+        $scope.popover = popover;
+    });
+
+    $scope.showba = function () {
+      alert("haode");
+    };
+
+
+    $scope.openPopover = function ($event) {
+        $scope.popover.show($event);
+    };
+
+    $scope.colsePopover = function ($event) {
+        $scope.popover.hide();
+    };
+
+    $scope.questions = [
+        {question: "无安全问题"},
+        {question: "自定义问题"},
+        {question: "我爸爸的出生地"},
+        {question: "我的小学校名"},
+        {question: "我的中学校名"},
+        {question: "我最喜欢的运动"},
+        {question: "我最喜欢的歌曲"},
+        {question: "我最喜欢的电影"},
+        {question: "我最喜欢的颜色"},
+        {question: "我的大学校名"},
+        {question: "我的手机号码"},
+        {question: "我的爱人姓名"},
+        {question: "我家的邮政编码"},
+        {question: "我最喜欢的论坛"},
+        {question: "我最最不喜欢的论坛"},
+        {question: "我最喜欢的动画"}
+
+
+    ];
+
+
+
 
   });
