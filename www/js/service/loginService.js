ffcdf7338340b6edf97a446ad84f3eb45fad993b
DESKTOP-TTIC7FS\jin <agoodcoolman@gmail.com>
2017-07-22 16:08:57 +0800

    1.模拟表单提交

--- www/js/service/loginService.js
+++ www/js/service/loginService.js
@@ -6,21 +6,17 @@ angular.module('mjm.loginService', ['Encrypt'])
 .factory('$loginFactory', function (ENV, $http, Md5) {
     var loginFactory = {};
     loginFactory.login = function (username, password, securityQuestion, answer) {
+
       $http({
         method:'POST',
         url:'http://www.majiamen.com/login.php',
-        data:{
-          lgt:"0",
-          pwuser:"小金",
-          pwpwd:Md5.hex_md5("1jinmingkai"),
-          question:"-1",
-          customquest:"我叫什么名字",
-          cktime:3600,
-          answer:"金明凯"
-        }
+        data:'forward=&jumpurl=http%3A%2F%2Fwww.majiamen.com%2F&step=2&lgt=0&pwuser=%D0%A1%BD%F0&pwpwd=1jinmingkai&question=-1&customquest=%CE%D2%BD%D0%CA%B2%C3%B4%C3%FB%D7%D6&answer=%BD%F0%C3%F7%BF%AD&hideid=0&cktime=31536000'
+          ,
+        headers:{'Content-Type': 'application/x-www-form-urlencoded'}
       }).then(function (response) {
         // alert("login1");
-        return response;
+        console.info(response.data);
+        return response.data;
       }, function (error) {
         // alert("login2");
         return error;
